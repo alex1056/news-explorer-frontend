e8ff1baf60c527f500e93c733bab5574d0f75678
Alexander1056 <gituser1056@gmail.com>
2020-08-16 23:06:23 +0300

    Настройки webpack для одной точки сборки

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/no-dynamic-require': 'off',
+    'no-underscore-dangle': [2, { allow: ['foo_', '_id'] }],
+    'func-names': ['error', 'never', { generators: 'as-needed' }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'global-require': 0,
+  },
+};
